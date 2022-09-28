3c823266fad566a666d4f32d5e93c6c955a9c3ba
Your Name <you@example.com>
2022-09-28 12:27:54 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 // Iteration 1: Names and Input
 //
+const hacker1 = "XXXX";
+console.log(`the driver's name is ${hacker1}`);
+
+const hacker2 = "YYYY";
+console.log(`the navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
 
+if (hacker1.length > hacker2.length){
+    console.log(`the driver's name is ${hacker1.length} characters.`)
+}
+else if(hacker1.length < hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+else{
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+};
 
 // Iteration 3: Loops
+
+
+
+//3.2
+const hacker1reves = hacker1.split('').reverse().join('')
+console.log({hacker1reves})
